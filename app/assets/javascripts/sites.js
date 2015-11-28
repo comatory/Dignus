d0f9f70c1d67d924a8c104e763f79dd8b385bd7a
Ondrej Synacek <ondrej.synacek@gmail.com>
2015-11-28 12:05:00 +0100

    more css on front page

---
+++ app/assets/javascripts/sites.js
@@ -0,0 +1,10 @@
+$(document).ready(function() {
+
+    $('div#show-organizer-more').click(function() {
+        $('div#organizer-more').slideToggle("slow");
+    });
+
+    $('div#show-performer-more').click(function() {
+        $('div#performer-more').slideToggle("slow")
+    });
+});
