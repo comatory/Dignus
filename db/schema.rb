84a65cfc9903d7054ad6b05d0c8c7cf76b7a32c1
Ondrej Synacek <ondrej.synacek@gmail.com>
2015-11-29 20:00:17 +0100

    final database fix

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151129183802) do
+ActiveRecord::Schema.define(version: 20151127114017) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -48,12 +48,12 @@ ActiveRecord::Schema.define(version: 20151129183802) do
     t.string   "venue"
     t.datetime "created_at",          null: false
     t.datetime "updated_at",          null: false
-    t.datetime "start_time"
-    t.datetime "end_time"
     t.string   "poster_file_name"
     t.string   "poster_content_type"
     t.integer  "poster_file_size"
     t.datetime "poster_updated_at"
+    t.datetime "start_time"
+    t.datetime "end_time"
   end
 
   add_index "events", ["user_id"], name: "index_events_on_user_id", using: :btree
