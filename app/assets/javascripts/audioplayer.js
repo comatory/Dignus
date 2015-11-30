1ddb78ee2b51cf3f0c619f6e862dacd0181dbf8f
Ondrej Synacek <ondrej.synacek@gmail.com>
2015-11-30 12:59:17 +0100

    modified audio player

--- app/assets/javascripts/audioplayer.js
+++ app/assets/javascripts/audioplayer.js
@@ -3,12 +3,21 @@ $(document).ready(function() {
     $('div.track').click(function(e) {
         var trackDiv = $(e.target);
         var trackAudio = $(e.target).children()[0];
+        var trackDuration = trackAudio.duration;
+        var seekBar = $(trackDiv).children().last().children()[0];
+
         if (trackAudio.paused == true) {
             trackAudio.play();
-            trackDiv.css({'background-color': 'white', 'color': 'black'})
+            trackDiv.addClass('track-playing');
+            $(seekBar).attr('max', trackDuration);
+
+            $(trackAudio).on('timeupdate', function() {
+                $(seekBar).attr('value', trackAudio.currentTime);
+            })
+
         } else {
             trackAudio.pause();
-            trackDiv.css({'background-color': 'grey', 'color': 'black'})
+            trackDiv.removeClass('track-playing');
         }
     });
 })
--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -2,4 +2,10 @@ module UsersHelper
   def can_invite_to_event?
     current_user.organizer && params[:id].to_i != current_user.id && User.find_by(id: params[:id].to_i).performer
   end
+
+  def embed(youtube_url)
+    youtube_id = youtube_url.split("=").last
+    content_tag(:iframe, nil, src: "//www.youtube.com/embed/#{youtube_id}")
+  end
+
 end
