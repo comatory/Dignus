1fbd280e1524df8a090e31cce70f159f9f9d764b
Ondrej Synacek <ondrej.synacek@gmail.com>
2015-12-10 21:43:44 +0100

    fix tests

--- spec/factories/event.rb
+++ spec/factories/event.rb
@@ -2,8 +2,8 @@ FactoryGirl.define do
   factory :event, class: 'Event' do
     name  Faker::Lorem.word
     description Faker::Lorem.sentence(3)
-    venue Faker::Address.city
     start_time Faker::Time.forward(3)
     end_time Faker::Time.forward(20)
+    location_id 1
   end
 end
