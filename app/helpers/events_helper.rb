67cb578a107048afe84ebeb62c3016de5fb2dd7a
Ondrej Synacek <ondrej.synacek@gmail.com>
2015-12-25 18:16:15 +0100

    edit event time picker constrains (JS)

--- app/helpers/events_helper.rb
+++ app/helpers/events_helper.rb
@@ -2,4 +2,18 @@ module EventsHelper
   def event_not_locked?(event)
     event.start_time > DateTime.now
   end
+
+  def event_start_time_min
+    start_time = DateTime.now + (4.0 / 24)
+    momentjs_formatted(start_time)
+  end
+
+  def event_end_time_min
+    end_time = DateTime.now + (8.0 / 24)
+    momentjs_formatted(end_time)
+  end
+
+  def momentjs_formatted(time)
+    time.strftime("%Y-%m-%d %H:%M")
+  end
 end
